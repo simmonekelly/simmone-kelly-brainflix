9e32c49251d8076333c2d567a8f43faaff27949f
Simmone Kelly <simmone.kelly@gmail.com>
2022-04-28 19:06:40 -0400

    cleaning up file structure

--- client/src/components/Home-Page/HomePage.js
+++ client/src/components/Home-Page/HomePage.js
@@ -35,10 +35,29 @@ export default class HomePage extends Component {
             console.log(error)
         });
     }
-  
-    // onclick event listener to get clicked element id
-    getNewId = (id) => {
-        this.setState({videoId: id});
+
+    //runs whenever state is set
+    componentDidUpdate(prevProps, prevState) {
+        
+        //checks if current state videoId and comments length matches previous state to fetch video info
+        if ((this.state.videoId !== prevState.videoId) || (this.state.videoInfo.comments.length !== prevState.videoInfo.comments.length)) {
+            this.fetchVideoInfo(this.state.videoId);
+        }
+        
+        //checks to see if id is available from params
+        if(this.props.match.params.id) {
+            const videoId = this.props.match.params.id;
+            
+            //checks to see if id param matches previous state id to then set state
+            if (prevState.videoId !== videoId) {
+                this.setState({videoId: videoId})
+            }
+        } else if (!this.props.match.params.id && prevProps.match.params.id){
+            this.setState({
+                videoId: this.state.VideoData[0].id,
+            })
+        }
+        
     }
 
     // calls to get video info from axios
@@ -55,28 +74,10 @@ export default class HomePage extends Component {
         });
     }
 
-    componentDidUpdate(prevProps, prevState) {
-        
-        //checks if current state matches previous state to fetch video info
-        if ((this.state.videoId !== prevState.videoId) || (this.state.videoInfo.comments.length !== prevState.videoInfo.comments.length)) {
-            
-            this.fetchVideoInfo(this.state.videoId);
-        }
-        
-        //checks to see if id is available
-        if(this.props.match.params.id) {
-            const videoId = this.props.match.params.id;
-            
-            //checks to see if id param matches previous state id to then set state
-            if (prevState.videoId !== videoId) {
-                this.setState({videoId: videoId})
-            }
-        } else if (!this.props.match.params.id && prevProps.match.params.id){
-            this.setState({
-                videoId: this.state.VideoData[0].id,
-            })
-        }
-        
+    //passed functions to child components
+    // onclick event listener to get clicked element id
+    getNewId = (id) => {
+        this.setState({videoId: id});
     }
 
     //manages comment state change
@@ -115,7 +116,7 @@ export default class HomePage extends Component {
     render () {
 
     const currentVideoIndex = this.state.VideoData.findIndex(element => element.id === this.state.videoId);
-
+        console.log(this.state.videoInfo.comments)
     if (!this.state.videoId) {
         return null
     }
